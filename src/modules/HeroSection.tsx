dfc7095a7a743b744113d9ec6631bc6fc796092d
Austin Wopara <austinwopara@gmail.com>
2026-01-13 22:06:28 +0100

    complete hero section

--- src/modules/HeroSection.tsx
+++ src/modules/HeroSection.tsx
@@ -28,7 +28,7 @@ const HeroSection = () => {
           </button>
         </div>
         <motion.span
-          className="absolute left-[10%] top-7 h-16.75 w-16.75"
+          className="absolute left-[10%] top-7 w-16.75 h-16.75"
           animate={{ rotate: 180 }}
           transition={{ duration: 10, repeat: Infinity }}
         >
@@ -40,10 +40,39 @@ const HeroSection = () => {
           />
         </motion.span>
         <motion.span
-          className="absolute right-0 top-34 h-13 w-36"
+          className="absolute right-[10%] top-4 w-17.25 h-17.75"
           animate={{ y: ["0rem", "0.5rem", "0rem"] }}
           transition={{ y: { duration: 2, repeat: Infinity, repeatType: "loop" } }}
-        ></motion.span>
+        >
+          <Image
+            src={"bouncing-1.svg"}
+            alt="Bouncing Icon"
+            fill
+            style={{objectFit: "contain"}}
+          />
+        </motion.span>
+        <motion.span
+          className="absolute left-[16%] bottom-28 w-17.25 h-17.75"
+          animate={{ y: ["0.5rem", "0rem", "0.5rem"] }}
+          transition={{ y: { duration: 2, repeat: Infinity, repeatType: "loop" } }}
+        >
+          <Image
+            src={"bouncing-2.svg"}
+            alt="Bouncing Icon"
+            fill
+            style={{objectFit: "contain"}}
+          />
+        </motion.span>
+        <div
+          className="flex flex-col items-center gap-4 absolute right-[16%] bottom-36"
+        >
+          <motion.span
+            className="w-9.75 h-9.75 rounded-full bg-(--secondary)"
+            animate={{ y: ["0rem", "0.8rem", "0rem"] }}
+            transition={{ y: { duration: 1.5, repeat: Infinity, repeatType: "loop" } }}
+          ></motion.span>
+          <span className="w-15.75 h-4.25 bg-(--secondary)"></span>
+        </div>
       </div>
     </section>
   )
