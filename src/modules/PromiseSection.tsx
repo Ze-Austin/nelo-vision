a9b84fe818744c42e5c3bbb7c0dd8dee39590e91
Austin Wopara <austinwopara@gmail.com>
2026-01-15 19:16:59 +0100

    add ticket section

--- src/modules/PromiseSection.tsx
+++ src/modules/PromiseSection.tsx
@@ -28,7 +28,7 @@ const PromiseSection = () => {
         <div
           className="flex gap-2 lg:gap-4 bg-background rounded-md px-4 lg:px-6 py-2 lg:py-4"
         >
-          <span className="icon-wrapper w-6 h-4.25 lg:w-10 lg:h-8">
+          <span className="icon-wrapper w-8 h-4.25 lg:w-12 lg:h-8">
             <GreenTick />
           </span>
           <div className="flex flex-col gap-1">
@@ -73,7 +73,7 @@ const PromiseSection = () => {
         <div
           className="flex gap-2 lg:gap-4 bg-background rounded-md px-4 lg:px-6 py-2 lg:py-4"
         >
-          <span className="icon-wrapper w-6 h-4.25 lg:w-10 lg:h-8">
+          <span className="icon-wrapper w-8 h-4.25 lg:w-12 lg:h-8">
             <GreenTick />
           </span>
           <div className="flex flex-col gap-1">
--- src/modules/TicketSection.tsx
+++ src/modules/TicketSection.tsx
@@ -1,12 +1,270 @@
+import { BlackTick } from "@/theme/icons";
+
 const TicketSection = () => {
   return (
     <section
       id="tickets"
-      className="bg-[#E2F1E4] flex flex-col items-center gap-4 lg:gap-8 px-6 lg:px-16 py-8 lg:py-16"
+      className="bg-[#E2F1E4] flex flex-col items-center gap-4 lg:gap-8 px-6 lg:px-24 py-8 lg:py-16"
     >
       <h4 className="text-center">
         VISIONEERING 2026 TICKETS
       </h4>
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-8 lg:gap-6 w-full">
+        <div
+          className="flex flex-col items-center justify-between h-112 lg:h-184 p-6 lg:p-10 bg-background rounded-[10px]"
+        >
+          <div
+            className="flex flex-col gap-4 lg:gap-6 w-full"
+          >
+            <div className="flex justify-between">
+              <div className="flex flex-col justify-between">
+                <div className="flex flex-col gap-1">
+                  <span className="text-ticket-hd">
+                    Visioneering 2026 Retreat
+                  </span>
+                  <span
+                    className="flex items-center justify-center btn-ticket rounded-sm bg-(--orange)"
+                  >
+                    In-Person Access
+                  </span>
+                </div>
+                <span className="text-ticket-pr-lg pb-1.5">
+                  N90,000/$90
+                </span>
+              </div>
+              <div className="flex flex-col gap-4">
+                <div className="flex flex-col">                  
+                  <span className="text-ticket-dt">
+                    Saturday,
+                  </span>
+                  <span className="text-ticket-dt">
+                    24th January, 2026
+                  </span>
+                </div>
+                <div
+                  className="bg-(--bgTint) flex flex-col rounded-sm px-3 lg:px-4 py-1.5 lg:py-2"
+                >
+                  <span className="text-ticket-pr-sm">
+                    Early Bird
+                  </span>
+                  <span className="text-ticket-pr-md">
+                    N80,000/$80
+                  </span>
+                </div>
+              </div>
+            </div>
+            <hr className="border-(--borderColor)" />
+            <div className="flex flex-col w-full">
+              <div className="flex gap-1.5">
+                <span className="w-5.75 lg:w-8 h-4 lg:h-6 pt-0.5 lg:pt-1.25">
+                  <span className="icon-wrapper">
+                    <BlackTick />
+                  </span>
+                </span>
+                <span className="text-ticket-bd">
+                  Full breakthrough coaching experience with Nelo Nwokike (N.N)
+                </span>
+              </div>
+              <div className="flex gap-1.5">
+                <span className="w-4 lg:w-6 h-4 lg:h-6 pt-0.5 lg:pt-1.25">
+                  <span className="icon-wrapper">
+                    <BlackTick />
+                  </span>
+                </span>
+                <span className="text-ticket-bd">
+                  Expert led masterclasses
+                </span>
+              </div>
+              <div className="flex gap-1.5">
+                <span className="w-4 lg:w-6 h-4 lg:h-6 pt-0.5 lg:pt-1.25">
+                  <span className="icon-wrapper">
+                    <BlackTick />
+                  </span>
+                </span>
+                <span className="text-ticket-bd">
+                  Q & A with Expert Mentors
+                </span>
+              </div>
+              <div className="flex gap-1.5">
+                <span className="w-4 lg:w-6 h-4 lg:h-6 pt-0.5 lg:pt-1.25">
+                  <span className="icon-wrapper">
+                    <BlackTick />
+                  </span>
+                </span>
+                <span className="text-ticket-bd">
+                  In-person access to Visioneering 2026 Retreat
+                </span>
+              </div>
+              <div className="flex gap-1.5">
+                <span className="w-4 lg:w-6 h-4 lg:h-6 pt-0.5 lg:pt-1.25">
+                  <span className="icon-wrapper">
+                    <BlackTick />
+                  </span>
+                </span>
+                <span className="text-ticket-bd">
+                  Tea break and lunch
+                </span>
+              </div>
+              <div className="flex gap-1.5">
+                <span className="w-4 lg:w-6 h-4 lg:h-6 pt-0.5 lg:pt-1.25">
+                  <span className="icon-wrapper">
+                    <BlackTick />
+                  </span>
+                </span>
+                <span className="text-ticket-bd">
+                  Your personal goody bag
+                </span>
+              </div>
+              <div className="flex gap-1.5">
+                <span className="w-4.75 lg:w-6.75 h-4 lg:h-6 pt-0.5 lg:pt-1.25">
+                  <span className="icon-wrapper">
+                    <BlackTick />
+                  </span>
+                </span>
+                <span className="text-ticket-bd">
+                  Workbook + Supporting Resources + Tools + Templates
+                </span>
+              </div>
+              <div className="flex gap-1.5">
+                <span className="w-4 lg:w-6 h-4 lg:h-6 pt-0.5 lg:pt-1.25">
+                  <span className="icon-wrapper">
+                    <BlackTick />
+                  </span>
+                </span>
+                <span className="text-ticket-bd">
+                  6 months access to Visioneering 2026 replay
+                </span>
+              </div>
+              <div className="flex gap-1.5">
+                <span className="icon-wrapper w-4 lg:w-6 h-3 lg:h-4.75"></span>
+                <span className="text-ticket-ft">
+                  * Early bird discounted offer till 21st January
+                </span>
+              </div>
+            </div>
+          </div>
+          <button type="button" className="btn btn-lg">
+            Get your ticket
+          </button>
+        </div>
+        <div
+          className="flex flex-col items-center justify-between h-112 lg:h-184 p-6 lg:p-10 bg-background rounded-[10px]"
+        >
+          <div
+            className="flex flex-col gap-4 lg:gap-6 w-full"
+          >
+            <div className="flex justify-between">
+              <div className="flex flex-col justify-between">
+                <div className="flex flex-col gap-1">
+                  <span className="text-ticket-hd">
+                    Visioneering 2026 Retreat
+                  </span>
+                  <span
+                    className="flex items-center justify-center btn-ticket rounded-sm bg-(--orange)"
+                  >
+                    Virtual Access
+                  </span>
+                </div>
+                <span className="text-ticket-pr-lg pb-1.5">
+                  N70,000/$70
+                </span>
+              </div>
+              <div className="flex flex-col gap-4">
+                <div className="flex flex-col">                  
+                  <span className="text-ticket-dt">
+                    Saturday,
+                  </span>
+                  <span className="text-ticket-dt">
+                    24th January, 2026
+                  </span>
+                </div>
+                <div
+                  className="bg-(--bgTint) flex flex-col rounded-sm px-3 lg:px-4 py-1.5 lg:py-2"
+                >
+                  <span className="text-ticket-pr-sm">
+                    Early Bird
+                  </span>
+                  <span className="text-ticket-pr-md">
+                    N60,000/$60
+                  </span>
+                </div>
+              </div>
+            </div>
+            <hr className="border-(--borderColor)" />
+            <div className="flex flex-col w-full">
+              <div className="flex gap-1.5">
+                <span className="w-5.75 lg:w-8 h-4 lg:h-6 pt-0.5 lg:pt-1.25">
+                  <span className="icon-wrapper">
+                    <BlackTick />
+                  </span>
+                </span>
+                <span className="text-ticket-bd">
+                  Full breakthrough coaching experience with Nelo Nwokike (N.N)
+                </span>
+              </div>
+              <div className="flex gap-1.5">
+                <span className="w-4 lg:w-6 h-4 lg:h-6 pt-0.5 lg:pt-1.25">
+                  <span className="icon-wrapper">
+                    <BlackTick />
+                  </span>
+                </span>
+                <span className="text-ticket-bd">
+                  Expert led masterclasses
+                </span>
+              </div>
+              <div className="flex gap-1.5">
+                <span className="w-4 lg:w-6 h-4 lg:h-6 pt-0.5 lg:pt-1.25">
+                  <span className="icon-wrapper">
+                    <BlackTick />
+                  </span>
+                </span>
+                <span className="text-ticket-bd">
+                  Q & A with Expert Mentors
+                </span>
+              </div>
+              <div className="flex gap-1.5">
+                <span className="w-4 lg:w-6 h-4 lg:h-6 pt-0.5 lg:pt-1.25">
+                  <span className="icon-wrapper">
+                    <BlackTick />
+                  </span>
+                </span>
+                <span className="text-ticket-bd">
+                  Online access to Visioneering 2026 Retreat
+                </span>
+              </div>
+              <div className="flex gap-1.5">
+                <span className="w-4.75 lg:w-6.75 h-4 lg:h-6 pt-0.5 lg:pt-1.25">
+                  <span className="icon-wrapper">
+                    <BlackTick />
+                  </span>
+                </span>
+                <span className="text-ticket-bd">
+                  Workbook + Supporting Resources + Tools + Templates
+                </span>
+              </div>
+              <div className="flex gap-1.5">
+                <span className="w-4 lg:w-6 h-4 lg:h-6 pt-0.5 lg:pt-1.25">
+                  <span className="icon-wrapper">
+                    <BlackTick />
+                  </span>
+                </span>
+                <span className="text-ticket-bd">
+                  6 months access to Visioneering 2026 replay
+                </span>
+              </div>
+              <div className="flex gap-1.5">
+                <span className="icon-wrapper w-4 lg:w-6 h-3 lg:h-4.75"></span>
+                <span className="text-ticket-ft">
+                  * Early bird discounted offer till 21st January
+                </span>
+              </div>
+            </div>
+          </div>
+          <button type="button" className="btn btn-lg">
+            Get your ticket
+          </button>
+        </div>
+      </div>
     </section>
   )
 };
